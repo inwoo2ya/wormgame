0c906c51f48a990a1ee0f6e06c7a4c8ed3d12442
Jeon Jae Ho <jaehoo8936@naver.com>
2022-03-31 14:47:40 +0900

    Update WebController.java

--- wormgame/src/main/java/com/capstonedesign07/wormgame/controller/WebController.java
+++ wormgame/src/main/java/com/capstonedesign07/wormgame/controller/WebController.java
@@ -1,18 +1,22 @@
 package com.capstonedesign07.wormgame.controller;
 
 import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 public class WebController {
     @RequestMapping("/")
-    public String index() {
+    public String root() {
         return "index";
     }
 
-    @RequestMapping("/jsp")
-    public String jspPage() {
+    @RequestMapping("/index")
+    public String index() {
         return "index";
+    }
+    
+    @RequestMapping("/Game")
+    public String Game() {
+        return "Game";
     }
 }
