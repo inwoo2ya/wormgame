a66cbbbb325780ae71ec2c809817dc6892954312
Jeon Jae Ho <jaehoo8936@naver.com>
2022-04-10 23:55:54 +0900

    Create UserService.java

---
+++ wormgame/src/main/java/com/capstonedesign07/wormgame/service/UserService.java
@@ -0,0 +1,43 @@
+package com.capstonedesign07.wormgame.service;
+
+import com.capstonedesign07.wormgame.domain.User;
+import com.capstonedesign07.wormgame.repository.MemoryUserRepository;
+import com.capstonedesign07.wormgame.repository.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import javax.servlet.http.HttpSession;
+import java.util.List;
+import java.util.Optional;
+
+@Service
+public class UserService {
+
+    private final UserRepository userRepository;
+
+    @Autowired
+    public UserService(UserRepository userRepository) {
+        this.userRepository = userRepository;
+    }
+
+    public String join(User user, HttpSession httpSession) {
+        validateDuplicateUser(user);
+        userRepository.save(user, httpSession);
+        return user.getSessionId();
+    }
+
+    private void validateDuplicateUser(User user) {
+        userRepository.findBySessionId(user.getName())
+                .ifPresent(u -> {
+                    throw new IllegalStateException("이미 존재하는 세션입니다.");
+                });
+    }
+
+    public List<User> findMembers() {
+        return userRepository.findAll();
+    }
+
+    public Optional<User> findOne(String userSessionId) {
+        return userRepository.findBySessionId(userSessionId);
+    }
+}
