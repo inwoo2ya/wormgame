7d103807ee920ce54de7f73cea5fad9398bd7317
inwoo2ya <86223023+inwoo2ya@users.noreply.github.com>
2022-04-11 01:00:41 +0900

    Add files via upload
    게임 방에서 방제목바꾸기 추가

--- wormgame/src/main/webapp/GamePlay.js
+++ wormgame/src/main/webapp/GamePlay.js
@@ -1,26 +1,6 @@
 
-function Gametitle(){
-    var title = document.getElementById('roomname');
-    title.appendChild(inputString);
-}
 function back(){
     window.location.href="Game.html";
-}
-function chatting(){
-    var form = document.getElementById('form');
-    var input = document.getElementById('input');
-    form.addEventListener('submit',function(e){
-        e.preventDefault();
-        var msg = input.value;
-
-        if(msg){
-            repo.textContent = msg;
-            form.reset();
-        } else{
-
-
-        }
-    })
 }
 function backindex(){
     window.location.href="index.html";
@@ -29,13 +9,24 @@ function backindex(){
 function tourl() { 
     window.location.href = "Game.html";
 }
-function createGame(){
-    var inputString =prompt('방 이름을 입력하세요!','방 제목');
-        if (inputString){
-            Play();
-        }
-}
-function Play(){
+
+function Play(inputString){
     window.location.href = "GamePlay.html";
+    const title = document.getElementById("roomname");
+    title.innerHTML=inputString;
                         
     }
+    function createGame(){
+        var inputString = prompt('방 이름을 입력하세요!','방 제목');
+        String(inputString);
+        if (inputString){
+            Play(inputString);
+    
+        }
+    }
+function roomname(){
+    var inputString = prompt('방 이름을 입력하세요!','방 제목');
+    String(inputString);
+    const title = document.getElementById("roomname");
+    title.innerHTML = inputString;
+}
