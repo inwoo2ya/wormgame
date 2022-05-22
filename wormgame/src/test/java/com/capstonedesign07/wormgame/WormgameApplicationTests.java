6ec01fbfc5584736f9dce6dfca564caef61906c1
Jeon Jae Ho <jaehoo8936@naver.com>
2022-05-23 02:31:00 +0900

    feat(jaehoo1) : Users 클래스 removeUser() 구현

--- wormgame/src/test/java/com/capstonedesign07/wormgame/WormgameApplicationTests.java
+++ wormgame/src/test/java/com/capstonedesign07/wormgame/WormgameApplicationTests.java
@@ -3,9 +3,14 @@ package com.capstonedesign07.wormgame;
 import com.capstonedesign07.wormgame.domain.GameStatus;
 import com.capstonedesign07.wormgame.domain.User;
 import com.capstonedesign07.wormgame.domain.Users;
+import com.capstonedesign07.wormgame.repository.MemoryUserRepository;
+import com.capstonedesign07.wormgame.repository.UserRepository;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+
+import java.util.List;
+
 import static org.assertj.core.api.Assertions.*;
 import static org.junit.jupiter.api.Assertions.*;
 
@@ -47,5 +52,22 @@ class WormgameApplicationTests {
 		users.addUser(user2);
 		assertThatIllegalArgumentException().
 				isThrownBy(() -> users.findUserByName("test"));
+	}
+
+	@Test
+	@DisplayName("MemoryUserRepository 테스트")
+	void memoryUserRepositoryTest() {
+		UserRepository userRepository = new MemoryUserRepository();
+		User user1 = new User("SID1", "test1");
+		User user2 = new User("SID2", "test2");
+		userRepository.save(user1);
+		userRepository.save(user2);
+		assertAll(
+				() -> assertThat(userRepository.findAll().getSize()).isEqualTo(2),
+				() -> assertThat(userRepository.findBySessionId("SID2").getName()).isEqualTo("test2"),
+				() -> assertThat(userRepository.findByName("test1").getSessionId()).isEqualTo("SID1"),
+				() -> userRepository.delete(user1),
+				() -> assertThat(userRepository.findAll().getSize()).isEqualTo(1)
+		);
 	}
 }
