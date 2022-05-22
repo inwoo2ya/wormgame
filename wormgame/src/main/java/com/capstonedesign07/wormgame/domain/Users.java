6a61b9005ac4618f3530e5568168d0d0a2a392db
Jeon Jae Ho <jaehoo8936@naver.com>
2022-05-23 02:27:43 +0900

    feat(jaehoo1) : Users 클래스 removeUser() 구현

--- wormgame/src/main/java/com/capstonedesign07/wormgame/domain/Users.java
+++ wormgame/src/main/java/com/capstonedesign07/wormgame/domain/Users.java
@@ -9,18 +9,23 @@ public class Users {
 
     private List<User> users = new ArrayList<>();
 
+    public int getSize() {
+        return users.size();
+    }
+
     public User findUserByIndex(int index) {
-        if(index >= users.size() || index < 0) {
-            throw new IllegalArgumentException("findUserByIndex : 잘못된 index 접근");
-        }
+        validateIndex(index);
         return users.get(index);
     }
 
     public User findUserBySessionId(String sessionId) {
         List<User> foundUser = IntStream.range(0, users.size())
-                .filter(i -> users.get(i).getName().equals(sessionId))
+                .filter(i -> users.get(i).getSessionId().equals(sessionId))
                 .mapToObj(users::get)
                 .collect(Collectors.toList());
+        if(foundUser.size() != 1) {
+            throw new IllegalArgumentException("findUserBySessionId : user가 여러명이거나 찾지 못함");
+        }
         return foundUser.get(0);
     }
 
@@ -37,5 +42,15 @@ public class Users {
 
     public void addUser(User user) {
         users.add(user);
+    }
+
+    public void removeUser(User user) {
+        users.remove(user);
+    }
+
+    private void validateIndex(int index) {
+        if(index >= users.size() || index < 0) {
+            throw new IllegalArgumentException("findUserByIndex : 잘못된 index 접근");
+        }
     }
 }
