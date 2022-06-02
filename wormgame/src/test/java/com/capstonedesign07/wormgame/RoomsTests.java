f56287a64de1c5673a36ef360e98c7270466a85c
jaehoo1 <jaehoo8936@naver.com>
2022-06-03 02:11:08 +0900

    test(jaehoo1) : RoomsTests 기능별로 분리

--- wormgame/src/test/java/com/capstonedesign07/wormgame/RoomsTests.java
+++ wormgame/src/test/java/com/capstonedesign07/wormgame/RoomsTests.java
@@ -1,6 +1,6 @@
 package com.capstonedesign07.wormgame;
 
-import com.capstonedesign07.wormgame.domain.RoomStatus;
+import com.capstonedesign07.wormgame.domain.Room;
 import com.capstonedesign07.wormgame.domain.Rooms;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
@@ -17,8 +17,15 @@ public class RoomsTests {
         Rooms rooms = new Rooms("test Room Name");
         assertAll(
                 () -> assertThat(rooms.getRooms()).hasSize(8),
-                () -> assertThat(rooms.findRoomByIndex(1).getName()).isEqualTo("test Room Name 2"),
-                () -> assertThat(rooms.findRoomByName("test Room Name 5").getRoomStatus()).isEqualByComparingTo(RoomStatus.WAIT)
+                () -> assertThat(rooms.findRoomByIndex(1).getName()).isEqualTo("test Room Name 2")
         );
+    }
+
+    @Test
+    @DisplayName("Rooms 이름으로 찾기 테스트")
+    void findRoomByNameTest() {
+        Rooms rooms = new Rooms("test Room Name");
+        Room room5 = rooms.getRooms().get(4);
+        assertThat(rooms.findRoomByName("test Room Name 5")).isEqualTo(room5);
     }
 }
