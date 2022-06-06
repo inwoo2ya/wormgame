2e0d0b873bd3828cc212e0e553097de56532550f
jaehoo1 <jaehoo8936@naver.com>
2022-06-06 17:32:07 +0900

    feat(jaehoo1) : Bomb를 객체가 있는 위치에 설정하지 못하게 하는 기능

--- wormgame/src/main/java/com/capstonedesign07/wormgame/domain/Bomb.java
+++ wormgame/src/main/java/com/capstonedesign07/wormgame/domain/Bomb.java
@@ -5,7 +5,10 @@ public class Bomb {
     private Position position;
     private boolean alive;
 
-    public Bomb(Position position) {
+    public Bomb(Position position, char[][] userBoard) {
+        if (userBoard[position.getX()][position.getY()] != 0) {
+            throw new IllegalArgumentException("폭탄 위치에 다른 객체가 있음");
+        }
         this.position = position;
         this.alive = true;
     }
--- wormgame/src/test/java/com/capstonedesign07/wormgame/BombTests.java
+++ wormgame/src/test/java/com/capstonedesign07/wormgame/BombTests.java
@@ -2,9 +2,13 @@ package com.capstonedesign07.wormgame;
 
 import com.capstonedesign07.wormgame.domain.Bomb;
 import com.capstonedesign07.wormgame.domain.Position;
+import com.capstonedesign07.wormgame.domain.Worm;
+import com.capstonedesign07.wormgame.domain.Worms;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import java.util.ArrayList;
+import java.util.List;
 import static org.assertj.core.api.Assertions.*;
 import static org.junit.jupiter.api.Assertions.*;
 
@@ -14,12 +18,28 @@ public class BombTests {
     @Test
     @DisplayName("Bomb 생성 테스트")
     void createBombTest() {
-        Bomb bomb = new Bomb(new Position(0, 0));
+        Bomb bomb = new Bomb(new Position(0, 0), new char[Position.BOARD_SIZE][Position.BOARD_SIZE]);
         Position bombPosition = bomb.getPosition();
         assertAll(
                 () -> assertThat(bombPosition.getX()).isEqualTo(0),
                 () -> assertThat(bombPosition.getY()).isEqualTo(0),
                 () -> assertThat(bomb.isAlive()).isTrue()
         );
+    }
+
+    @Test
+    @DisplayName("지렁이가 Bomb와 위치가 겹치는 경우 테스트")
+    void bombOverlappedWithWormTest() {
+        char[][] userBoard = new char[Position.BOARD_SIZE][Position.BOARD_SIZE];
+        Worm worm1 = new Worm(new Position(0, 0), 2);
+        Worm worm2 = new Worm(new Position(1, 1), 2);
+        Worm worm3 = new Worm(new Position(2, 2), 2);
+        List<Worm> threeWorms = new ArrayList<>();
+        threeWorms.add(worm1);
+        threeWorms.add(worm2);
+        threeWorms.add(worm3);
+        Worms worms = new Worms(threeWorms, userBoard);
+        assertThatIllegalArgumentException()
+                .isThrownBy(() -> new Bomb(new Position(2, 2), userBoard));
     }
 }
