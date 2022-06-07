eb3ad66ba1d40dd32b364358ef9be7361b1e186e
jaehoo1 <jaehoo8936@naver.com>
2022-06-08 01:31:07 +0900

    refactor(jaehoo1) : webSocket.js 리팩터링

--- wormgame/src/main/webapp/webSocket.js
+++ wormgame/src/main/webapp/webSocket.js
@@ -3,15 +3,8 @@ let roomName = document.getElementById("Roomname").textContent;
 let userName = sessionStorage.getItem("userName");
 let sessionId = sessionStorage.getItem("sessionId");
 
-let player1Name;
-let player2Name;
-let player3Name;
-let player4Name;
-
-let player1SessionId;
-let player2SessionId;
-let player3SessionId;
-let player4SessionId;
+let playerName = [];
+let playerSessionId = [];
 
 let playerCount;
 let isGamePlaying = false;
@@ -71,26 +64,14 @@ function gameStartSend() {
 }
 
 function currentRoomPlayer(str) {
-    if (!str.indexOf("EVENT_PLAYER_NAME1"))
-        player1Name = str.substr(21);
-    else if (!str.indexOf("EVENT_PLAYER_NAME2"))
-        player2Name = str.substr(21);
-    else if (!str.indexOf("EVENT_PLAYER_NAME3"))
-        player3Name = str.substr(21);
-    else if (!str.indexOf("EVENT_PLAYER_NAME4"))
-        player4Name = str.substr(21);
-    else if (!str.indexOf("EVENT_PLAYER_SESSIONID1"))
-        player1SessionId = str.substr(26);
-    else if (!str.indexOf("EVENT_PLAYER_SESSIONID2"))
-        player2SessionId = str.substr(26);
-    else if (!str.indexOf("EVENT_PLAYER_SESSIONID3"))
-        player3SessionId = str.substr(26);
-    else if (!str.indexOf("EVENT_PLAYER_SESSIONID4"))
-        player4SessionId = str.substr(26);
+    if(!str.indexOf("EVENT_PLAYER_NAME"))
+        playerName[str[17]] = str.substr(21);
+    else if(!str.indexOf("EVENT_PLAYER_SESSIONID"))
+        playerSessionId[str[22]] = str.substr(26);
     else if (!str.indexOf("EVENT_PLAYER_COUNT"))
         playerCount = str.substr(21);
     
-    if (sessionId == player1SessionId && !isGamePlaying && playerCount > 1)
+    if (sessionId == playerSessionId[1] && !isGamePlaying && playerCount > 1)
         startBtnToggle(true);
     else
         startBtnToggle(false);
